f12e420d961ea81c37ad0947f1ae22a26dc4efbf
GOURANGA PAL <121525554+Gouranga02@users.noreply.github.com>
2026-06-01 16:17:55 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -728,4 +728,11 @@ if __name__ == '__main__':
     print(f"   • Model: {MODEL}")
     print("\n" + "=" * 70 + "\n")
 
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host='0.0.0.0',
+        port=port,
+        debug=False
+    )
